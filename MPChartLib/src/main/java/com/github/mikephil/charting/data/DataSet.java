dde2a27b012bcdef40e7371aade5a6b2d2b1ead9
PawelErnestLuczak <pawel.ernest.luczak@gmail.com>
2017-07-04 15:09:01 +0200

    format fix

--- MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
@@ -291,7 +291,7 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
 
     @Override
     public T getEntryForIndex(int index) {
-		if(index<0)
+	if(index<0)
             return null;
         if(index>=mValues.size())
             return null;
