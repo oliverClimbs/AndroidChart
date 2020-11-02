PhilJay/MPAndroidChart

Removed mLabelRotatedWidth/Height variables from XAxis model.
Those 2 variables were used only if mLabelRotationAngle was different than 0.
In such situation mLabelWidth/Height became unused.
This was removes this unnecessary overhead - merges those two cases
into one making use of mLabelWidth/Height variables in XAxis.

MPChartExample/src/com/xxmassdeveloper/mpchartexample/BarChartActivity.java
@@ -212,6 +212,11 @@ public class BarChartActivity extends DemoBase implements OnSeekBarChangeListene
                             .show();
                 break;
             }
+            case R.id.actionRotateXAxisLabelsBy45Deg: {
+                mChart.getXAxis().setLabelRotationAngle(45);
+                mChart.notifyDataSetChanged();
+                mChart.invalidate();
+            }
         }
         return true;
     }

oliverClimbs/AndroidChart

MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/BarChartActivity.java
@@ -275,6 +275,11 @@ public class BarChartActivity extends DemoBase implements OnSeekBarChangeListene
                 }
                 break;
             }
+            case R.id.actionRotateXAxisLabelsBy45Deg: {
+                chart.getXAxis().setLabelRotationAngle(45);
+                chart.notifyDataSetChanged();
+                chart.invalidate();
+            }
         }
         return true;
     }