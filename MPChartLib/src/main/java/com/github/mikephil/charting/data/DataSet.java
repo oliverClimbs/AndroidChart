PhilJay/MPAndroidChart

safe get for entries

MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
@@ -283,6 +283,10 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
 
     @Override
     public T getEntryForIndex(int index) {
+		if(index<0)
+            return null;
+        if(index>=mValues.size())
+            return null;
         return mValues.get(index);
     }
 

oliverClimbs/AndroidChart

MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
@@ -78,7 +78,7 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
     public void calcMinMaxY(float fromX, float toX) {
         mYMax = -Float.MAX_VALUE;
         mYMin = Float.MAX_VALUE;
-        
+
         if (mEntries == null || mEntries.isEmpty())
             return;
 
@@ -181,7 +181,6 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
     public abstract DataSet<T> copy();
 
     /**
-     *
      * @param dataSet
      */
     protected void copy(DataSet dataSet) {
@@ -314,6 +313,10 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
 
     @Override
     public T getEntryForIndex(int index) {
+        if (index < 0)
+            return null;
+        if (index >= mEntries.size())
+            return null;
         return mEntries.get(index);
     }
 